7a79d88ff2eaaf8d74dbb1a9edfe1ab81aefe0ba
owenbcoding <eoghankb@gmail.com>
2025-02-21 15:39:31 +0000

    changed route from welcome to index

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'index')
+    ->name('index');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
