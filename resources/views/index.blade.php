c8a8caa1a936644ca9beab9f2f53a09e8c33ae3e
owenbcoding <eoghankb@gmail.com>
2025-03-03 18:16:27 +0000

    add responsiveness and add content for popular + most anticipated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-5">
             popular games
         </h2>
-        <div class="popular games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -235,15 +235,15 @@
                 <p>Play Station 5</p>
             </div>
         </div>
-        <div class="flex my-10">
-            <div class="recently-viewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-viewed w-full  lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">recently viewed</h2>
                 <div class="recently-reviews-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
                                 <img src="{{ Vite::asset('resources/img/ff7.png') }}" alt="game-cover"
-                                    class="hover:opacity-75 transition ease-in-out duration-150 w-48">
+                                    class="hover:opacity-75 transition ease-in-out duration-150 w-20 lg:w-48">
                             </a>
 
                             <!-- Score overlay -->
@@ -260,24 +260,172 @@
                                 Final Fantasy VII Remake
                             </a>
                             <div class="text-gray-400 mt-1">Play Station 5</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 A spectacular re-imagining of the most visionary games ever, Final Fantasy VII Remake
                                 rebuilds and expands the legendary RPG for today. The first game in the project will be
                                 set in electric city of Midgar and presents a fully standalone gaming experience.
                             </p>
                         </div>
                     </div>
-                    <div>Game Card</div>
-                    <div>Game Card</div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ Vite::asset('resources/img/doom.png') }}" alt="game-cover"
+                                    class="hover:opacity-75 transition ease-in-out duration-150 w-20 lg:w-48">
+                            </a>
+
+                            <!-- Score overlay -->
+                            <div class="absolute bottom-2 right-2 w-16 h-16 bg-gray-800 text-white rounded-full flex items-center justify-center"
+                                style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#"
+                                class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy VII Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">Play Station 5</div>
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                A spectacular re-imagining of the most visionary games ever, Final Fantasy VII Remake
+                                rebuilds and expands the legendary RPG for today. The first game in the project will be
+                                set in electric city of Midgar and presents a fully standalone gaming experience.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ Vite::asset('resources/img/Animal-crossing.png') }}" alt="game-cover"
+                                    class="hover:opacity-75 transition ease-in-out duration-150 w-20 lg:w-48">
+                            </a>
+
+                            <!-- Score overlay -->
+                            <div class="absolute bottom-2 right-2 w-16 h-16 bg-gray-800 text-white rounded-full flex items-center justify-center"
+                                style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#"
+                                class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy VII Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">Play Station 5</div>
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                A spectacular re-imagining of the most visionary games ever, Final Fantasy VII Remake
+                                rebuilds and expands the legendary RPG for today. The first game in the project will be
+                                set in electric city of Midgar and presents a fully standalone gaming experience.
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
-            <div class="most-anticipated w-1/4 mr-32">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur quis deleniti unde libero vitae atque
-                ullam itaque at eaque nostrum, eius dignissimos ab quasi? Dolorum corporis pariatur, explicabo eveniet
-                delectus neque est ad libero necessitatibus fuga voluptate voluptas error omnis provident eius vitae
-                vero
-                alias natus quaerat, praesentium fugit illo!
+            <div class="most-anticipated w-1/4 lg:w-1/4 mt-12 lg:mt-0">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Cyberpunk.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Marvels-avengers.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Marvel's Avengers</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 3, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Ghost_of_Tsushima.jpg') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Ghost of Tsushima</a>
+                            <div class="text-gray-400 text-sm mt-1">July 17, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/The-Last-of-Us-2.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">The Last of Us Part II</a>
+                            <div class="text-gray-400 text-sm mt-1">June 19, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-8">Comming Soon</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Cyberpunk.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Marvels-avengers.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Marvel's Avengers</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 3, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/Ghost_of_Tsushima.jpg') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Ghost of Tsushima</a>
+                            <div class="text-gray-400 text-sm mt-1">July 17, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/The-Last-of-Us-2.png') }}" alt="game-cover"
+                                class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">The Last of Us Part II</a>
+                            <div class="text-gray-400 text-sm mt-1">June 19, 2020</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
