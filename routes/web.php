ab56a4743b295e4a0cd9463261f27c2cb6aa28ff
owenbcoding <eoghankb@gmail.com>
2025-03-05 09:16:52 +0000

    removed dashbord and profile routes and created show route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'index')
     ->name('index');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::view('/show', 'show')
+    ->name('show');
 
 require __DIR__.'/auth.php';
