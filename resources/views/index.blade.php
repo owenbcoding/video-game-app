3205f302ccd91f7ea930092ab88e274ad44f2a2c
owenbcoding <eoghankb@gmail.com>
2025-04-06 15:03:51 +0100

    refactored frontend to use livewire components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,41 +11,10 @@
             </div>
             <div class="most-anticipated w-1/4 lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
-                @foreach ($mostAnticipated as $game)
-                    <div class="most-anticipated-container space-y-10 mt-8">
-                        <div class="game flex">
-                            <a href="#">
-                                <img src="{{ Str::replace('thumb', 'cover_small', $game['cover']['url'] ?? '') }}"
-                                    alt="game-cover" class="hover:opacity-75 transition ease-in-out duration-150 w-16">
-                            </a>
-                            <div class="ml-4">
-                                <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
-                                <div class="text-gray-400 text-sm mt-1">
-                                    {{ carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-
+                <livewire:most-anticipated />
 
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-8">Coming Soon</h2>
-                @foreach ($comingSoon as $game)
-                    <div class="comming-soon-container space-y-10 mt-8">
-                        <div class="game flex">
-                            <a href="#">
-                                <img src="{{ Str::replace('thumb', 'cover_small', $game['cover']['url'] ?? '') }}"
-    alt="game-cover" class="hover:opacity-75 transition ease-in-out duration-150 w-16">
-                            </a>
-                            <div class="ml-4">
-                                <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
-                                <div class="text-gray-400 text-sm mt-1">
-                                    {{ carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
+                <livewire:comming-soon />
             </div>
         </div>
     </div>
