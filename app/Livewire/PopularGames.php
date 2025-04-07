f21c244e312a7a67f693fd3be45c5000588726c1
owenbcoding <eoghankb@gmail.com>
2025-04-07 14:29:10 +0100

    implimented caching ( need to refactor later )

--- app/Livewire/PopularGames.php
+++ app/Livewire/PopularGames.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class PopularGames extends Component
 {
@@ -18,32 +20,37 @@ class PopularGames extends Component
 
     public function loadPopularGames()
     {
+        sleep(3); // Simulate a delay for loading data
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $response = Http::post('https://id.twitch.tv/oauth2/token', [
-            'client_id' => env('IGDB_CLIENT_ID'),
-            'client_secret' => env('IGDB_CLIENT_SECRET'),
-            'grant_type' => 'client_credentials',
-        ]);
+        $this->popularGames = Cache::remember('popular-games', 7, function () use ($before, $after) {
+            // Step 1: Get the access token
+            $response = Http::post('https://id.twitch.tv/oauth2/token', [
+                'client_id' => env('IGDB_CLIENT_ID'),
+                'client_secret' => env('IGDB_CLIENT_SECRET'),
+                'grant_type' => 'client_credentials',
+            ]);
 
-        $accessToken = $response->json()['access_token'];
+            $accessToken = $response->json()['access_token'];
 
-        $this->popularGames = Http::withHeaders([
-            'Client-ID' => env('IGDB_CLIENT_ID'),
-            'Authorization' => 'Bearer ' . $accessToken,
-        ])
-            ->withBody(
-                "fields name, cover.url, first_release_date, platforms.abbreviation, rating, summary;
+            // Step 2: Use the access token to fetch popular games
+            return Http::withHeaders([
+                'Client-ID' => env('IGDB_CLIENT_ID'),
+                'Authorization' => 'Bearer ' . $accessToken,
+            ])
+                ->withBody(
+                    "fields name, cover.url, first_release_date, platforms.abbreviation, rating, summary;
             where platforms = (48,49,130,6)
             & first_release_date > {$before}
             & first_release_date < {$after};
             sort rating desc;
             limit 11;",
-                'text/plain'
-            )
-            ->post('https://api.igdb.com/v4/games')
-            ->json();
+                    'text/plain'
+                )
+                ->post('https://api.igdb.com/v4/games')
+                ->json();
+        });
     }
 
     public function render()
