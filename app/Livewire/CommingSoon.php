2da273f46b3a6876e706ed415bcbbe7f1b15e58c
owenbcoding <eoghankb@gmail.com>
2025-04-14 17:33:38 +0100

    refactored api code to use caching and imported facades cache

--- app/Livewire/CommingSoon.php
+++ app/Livewire/CommingSoon.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use Livewire\Component;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class CommingSoon extends Component
 {
@@ -19,29 +20,31 @@ class CommingSoon extends Component
     { 
         $current = Carbon::now()->timestamp;
 
-        // $response = Http::post('https://id.twitch.tv/oauth2/token', [
-        //     'client_id' => env('IGDB_CLIENT_ID'),
-        //     'client_secret' => env('IGDB_CLIENT_SECRET'),
-        //     'grant_type' => 'client_credentials',
-        // ]);
+        $this->comingSoon = Cache::remember('most-anticipated', 7, function () use ($current) {
 
-        // $accessToken = $response->json()['access_token'];
+            $response = Http::post('https://id.twitch.tv/oauth2/token', [
+                'client_id' => env('IGDB_CLIENT_ID'),
+                'client_secret' => env('IGDB_CLIENT_SECRET'),
+                'grant_type' => 'client_credentials',
+            ]);
+            
+            $accessToken = $response->json()['access_token'];
 
-        // $this->comingSoon = Http::withHeaders([
-        //     'Client-ID' => env('IGDB_CLIENT_ID'),
-        //     'Authorization' => 'Bearer ' . $accessToken,
-        // ])
-        $this->comingSoon = Http::withHeaders(config('services.igdb'))
-        ->withBody(
-            "fields name, cover.url, first_release_date, platforms.abbreviation, rating, summary;
-            where platforms = (48,49,130,6)
-            & first_release_date > {$current};
-            sort first_release_date asc;
-            limit 4;",
-            'text/plain'
-        )
-        ->post('https://api.igdb.com/v4/games')
-        ->json();
+            return Http::withHeaders([ 
+                'Client-ID' => config('services.igdb.client_id'),
+                'Authorization' => 'Bearer ' . $accessToken,
+            ])->withBody(
+                "fields name, cover.url, first_release_date, platforms.abbreviation, rating, summary;
+                where platforms = (48,49,130,6)
+                & first_release_date > {$current};
+                sort first_release_date asc;
+                limit 4;",
+                'text/plain'
+            )
+            ->post('https://api.igdb.com/v4/games')
+            ->json();
+
+        });
     }
 
     public function render()
