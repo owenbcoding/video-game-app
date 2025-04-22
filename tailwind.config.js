23405ec3154abee8c7563dbea0198db926cd885e
owenbcoding <eoghankb@gmail.com>
2025-04-22 15:49:10 +0100

    add spacing to theme extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            spacing: { 
+                '44': '11rem',
+            }
         },
     },
 
