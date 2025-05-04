8e23502ba42ae16dab93e9de4f0b848123d87618
owenbcoding <eoghankb@gmail.com>
2025-05-04 21:18:44 +0100

    created route for game slug to use game controller

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\GamesController;
 
 Route::get('/', [GamesController::class, 'index'])->name('index');
 
-// Route::view('/', 'index')
-//     ->name('index');
+Route::get('/games/{slug}', [GamesController::class, 'show'])
+    ->name('games.show')
+    ->where('slug', '[a-zA-Z0-9-]+');
 
-// Route::view('/show', 'show')
-//     ->name('show');
+Route::view('/show', 'show')
+    ->name('show');
 
 require __DIR__.'/auth.php';
