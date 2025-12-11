845a458461f8723a057e9807ae65e58922a717e0
owenbcoding <eoghankb@gmail.com>
2025-12-11 05:05:08 +0000

    modified game-card component

--- resources/views/components/game-card.blade.php
+++ resources/views/components/game-card.blade.php
@@ -1,3 +1,26 @@
 <div>
     <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+    <div class="game mt-8">
+        <div class="relative inline-block">
+            <a href="{{ route('games.show', $game['slug'] ?? '') }}">
+                <img src="{{ $game['coverImageUrl'] }}" alt="game-cover"
+                    class="hover:opacity-75 transition ease-in-out duration-150 w-full">
+            </a>
+
+            <div class="absolute bottom-2 right-2 w-16 h-16 bg-gray-800 text-white rounded-full flex items-center justify-center"
+                style="right: -29px; bottom: -29px;">
+                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                    {{ $game['rating'] }}
+                </div>
+            </div>
+
+        </div>
+        <a href="{{ route('games.show', $game['slug'] ?? '') }}"
+            class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
+            {{ $game['name'] ?? 'Unknown Game' }}
+        </a>
+        <div class="text-gray-400 mt-1">
+            {{ $game['platforms'] }}
+        </div>
+    </div>
 </div>
